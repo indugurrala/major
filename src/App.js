5b48b5ea10ef4410def9cf0af4d36c93ce59dc73
varun <varun.mahankali39@gmail.com>
2022-06-10 22:48:09 +0530

    final

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <div className="container-fluid">
-                <a className="navbar-brand">KNOw PCOS</a>
+                <a className="navbar-brand">kNOw PCOS</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- src/components/Info.js
+++ src/components/Info.js
@@ -4,32 +4,63 @@ import axios from 'axios';
 
 function Info() {
   let {userObj,isSuccess,isError,isLoading,errMsg}=useSelector(state=>state.user);
-  const [users,setUsers]=useState([])
+  const [users,setUsers]=useState({})
+
   useEffect(()=>{
     console.log(userObj);
+    const updateusers=(data)=>{
+      console.log("data is ",data)
+      setUsers(data)
+    }
     axios.get(`http://localhost:4000/detailapi/getdetails/${userObj.name}`)
     .then(response=>{
       console.log(response)
       console.log(response.data.payload)
-      // for(let user in response.data.payload){
-      //   users.push({user:response.data.payload[user]})
-      // }
       if(response.data.payload!==undefined){
-        let data=[...users]
-        data.push(response.data.payload)
-        setUsers(data)
+        updateusers(response.data.payload)
       }
-      console.log(users)
     })
     .catch(err=>console.log("Error occurred in accessing details"))}
   ,[userObj])
+  console.log(users)
 
   return (
     <div>
-      {/* users.map((user)=>{
-
-      }) */}
-      Hii
+      <table className='table table-striped table-hover'>
+        <tbody>
+        <tr>
+          <td>Name</td>
+          <td>{users.name}</td>
+        </tr>
+        <tr>
+          <td>Age</td>
+          <td>{users.age}</td>
+        </tr>
+        <tr>
+          <td>Weight</td>
+          <td>{users.weight} </td>
+        </tr>
+        <tr>
+          <td>BMI Index</td>
+          <td>{users.bmi} </td>
+        </tr>
+        <tr>
+          <td>Occupation</td>
+          <td>{users.occupation} </td>
+        </tr>
+        <tr>
+          <td>Average sleeping hours</td>
+          <td>{users.sleeping}</td>
+        </tr>
+        <tr>
+          <td>Average working hours</td>
+          <td>{users.working} </td>
+        </tr>
+        </tbody>
+      </table>
+      {/* <div style={{width:"150px",height:"150px",display:'flex',flexDirection:'column',marginTop:'500px'}}>
+        <hr style={{width:'100%',height:'5px'}}/>
+      </div> */}
     </div>
   )
 }
